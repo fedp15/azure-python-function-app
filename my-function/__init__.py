22aa76507275c3855c4d66245580d13b83823d85
jmargutti <jacopo.margutti@gmail.com>
2020-10-20 17:33:00 +0200

    first commit

---
+++ my-function/__init__.py
@@ -0,0 +1,36 @@
+import datetime
+import azure.functions as func
+from azure.storage.blob import BlobServiceClient, BlobClient
+import logging
+import os
+
+
+def function():
+    """
+    my function, just copy-paste your script here
+    :return:
+    """
+
+    # load app configs (edit them in azure portal)
+    connection_to_storage = os.environ['AzureWebJobsStorage']
+    my_config = os.environ["my-config"]
+
+    # load some data from a blob storage
+    logging.info('connect to storage %s', connection_to_storage)
+    blob_service_client = BlobServiceClient.from_connection_string(connection_to_storage)
+    blob_client = blob_service_client.get_blob_client(container='models', blob='gsdmm_model.pickle')
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due')
+    try:
+        function()
+    except Exception as e:
+        logging.error('Error:')
+        logging.error(e)
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
